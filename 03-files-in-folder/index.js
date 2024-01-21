f45328a4deb5b20e09e9baccb477d948b1c223c0
Таша <craftsw0man@yandex.ru>
2024-01-22 00:10:18 +0300

    fix: fix solution 3 task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,25 +1,25 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const dir = './secret-folder';
-const dirPath = path.resolve(__dirname, dir);
+const dirPath = path.join(__dirname, dir);
 fs.readdir(dirPath, { withFileTypes: true }, (error, items) => {
     if (error) {
         console.error(error.message);
-    } else {
-        const files = items.filter(elem => elem.isFile());
-        files.forEach(file => {
-             const pathFile = path.join(file.path, file.name);
-             const extname = path.extname(pathFile).slice(1);
-             const name = file.name.replace(`.${extname}`, '')
-             fs.stat(pathFile, (error, stats) => {
-                if (error) {
-                  console.log(error.message);
-                }
-                console.log(stats);
-        
-                console.log(`${name} - ${extname} - ${(stats.size / 1024).toFixed(3)}kb`);
-              });
-
-        })
+        return;
     }
+
+    const files = items.filter(elem => elem.isFile());
+    files.forEach(file => {
+        const pathFile = path.join(file.path, file.name);
+        const extname = path.extname(pathFile).slice(1);
+        const name = file.name.replace(`.${extname}`, '')
+        fs.stat(pathFile, (error, stats) => {
+            if (error) {
+                console.log(error.message);
+                return;
+            }
+            console.log(`${name} - ${extname} - ${(stats.size / 1024).toFixed(3)}kb`);
+        });
+    })
+
 });
